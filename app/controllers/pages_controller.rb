97a8e4cf2332524798edc0f0198ddde45723d1d4
Askin Akhan <askinakhan@gmail.com>
2015-02-26 14:04:18 +0000

    Added sign up and login functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
-  def home	
-  end
-  def about	
-  end
-  def contact
-  end
+	def home	
+	end
+	def about	
+	end
+	def contact
+	end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for Heroku
+  # Set to actual host 
+  config.action_mailer.default_url_options = { host: 'ash-pinteresting.herokuapp.com'}
 end
